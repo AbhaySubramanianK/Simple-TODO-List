c41a3f92f4267319c7e1b8c31db01678714a1727
Abhay <abhayk97@gmail.com>
2018-08-15 07:48:21 +0530

    Second Commit

--- todo.js
+++ todo.js
@@ -1,22 +1,32 @@
-function todo(){
+function todo(todoList, label){
 	while(label){
-		var option = prompt("Enter your option: add, list, quit ? ");
+		var option = prompt("Enter your option: add, list, delete, quit ? ");
 		if(option=="add"){
 			var add = prompt("Enter your todo for today!");
 			todoList.push(add);
 		}
 		else if(option=="list")	{
 			console.log("Here is your todo for today!");
-			console.log(todoList);
+			console.log("****************");
+			todoList.forEach(function(str,i){
+				count++;
+				console.log(i+". "+str)
+			});
+			console.log("****************");
 
 		}
 		else if(option=="quit"){
 			console.log("Quitting, Thank you for using!");
 			label=false;
 		}
+		else if(option=="delete"){
+			var n = prompt("Which todo you want to delete?")
+			delete todoList.splice(n,1);
+		}
 	}
 
 }
 var todoList = [];
+var count=0
 var label=true;
-todo();
+todo(todoList,label);
